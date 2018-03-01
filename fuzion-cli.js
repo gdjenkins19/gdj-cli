ef46ffe47b0fff43d031028657e9344636d756be
Gary Dean Jenkins <gary.jenkins@klowd.com>
2018-03-01 11:41:14 -0700

    mysql and db-builder working

--- fuzion-cli.js
+++ fuzion-cli.js
@@ -1,26 +1,38 @@
 #!/usr/bin/env node
-
+const shell = require("shelljs");
 const argv = require("optimist")
-	.usage("--op [gdj1|gdj2]")
+	.usage("--op [mysql-reset|mysql-start|db-builder]")
 	.demand(["op"])
 	.argv;
 
-console.log(`fuzion-cli --op {}`);
+console.log(`fuzion-cli --op ${argv.op}`);
 
-switch(argv.op) {
-	case "gdj1":
-		const gdj1 = require("./lib/gdj-cli-1");
-		gdj1();
-		break;
-	case "gdj2":
-		const gdj1 = require("./lib/gdj-cli-2");
-		gdj2();
-		break;
-	default:
-		fuzionCliError(`fuzion-cli:ERROR unrecognized --op "${argv.op}"`);
-		break;
+const ops = argv.op.split('|');
+
+for(let i=0; i<ops.length; i++) {
+	console.log(`fuzion-cli --op ${ops[i]}`);
+	switch(ops[i]) {
+		case "db-builder":
+			const dbBuilder = require("./lib/db-builder");
+			dbBuilder();
+			break;
+		case "mysql-start":
+			shell.exec("sudo launchctl unload -F /Library/LaunchDaemons/com.oracle.oss.mysql.mysqld.plist");
+			shell.exec("docker run -it --rm -v lmysql:/var/lib/mysql -p 3306:3306 --name fuzion-mysql -e MYSQL_ROOT_PASSWORD=coffeetime -d mysql:5.7.20");
+			shell.exec("sleep 10");
+			break;
+		case "mysql-reset":
+			shell.exec("docker stop fuzion-mysql");
+			shell.exec("docker volume rm lmysql");
+			break;
+		default:
+			fuzionCliError(`fuzion-cli:ERROR unrecognized --op "${ops[i]}"`);
+			break;
+	}
 }
 
 function fuzionCliError(errorString) {
 	console.log(errorString);
 }
+
+
