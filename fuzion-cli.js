f59a11741057e3f80003a7e4edc515382fbe87c2
Gary Dean Jenkins <gary.jenkins@klowd.com>
2018-03-01 11:55:47 -0700

    mysql-ops

--- fuzion-cli.js
+++ fuzion-cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const shell = require("shelljs");
 const argv = require("optimist")
 	.usage("--op [mysql-reset|mysql-start|db-builder]")
 	.demand(["op"])
@@ -13,17 +12,13 @@ for(let i=0; i<ops.length; i++) {
 	console.log(`fuzion-cli --op ${ops[i]}`);
 	switch(ops[i]) {
 		case "db-builder":
-			const dbBuilder = require("./lib/db-builder");
-			dbBuilder();
+			require("./lib/db-builder")();
 			break;
 		case "mysql-start":
-			shell.exec("sudo launchctl unload -F /Library/LaunchDaemons/com.oracle.oss.mysql.mysqld.plist");
-			shell.exec("docker run -it --rm -v lmysql:/var/lib/mysql -p 3306:3306 --name fuzion-mysql -e MYSQL_ROOT_PASSWORD=coffeetime -d mysql:5.7.20");
-			shell.exec("sleep 10");
+			require("./lib/mysql-ops").start();
 			break;
 		case "mysql-reset":
-			shell.exec("docker stop fuzion-mysql");
-			shell.exec("docker volume rm lmysql");
+			require("./lib/mysql-ops").reset();
 			break;
 		default:
 			fuzionCliError(`fuzion-cli:ERROR unrecognized --op "${ops[i]}"`);
