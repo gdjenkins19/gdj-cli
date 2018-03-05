833ce22e275b335560a81fa519b3bacbc5d99b64
Gary Dean Jenkins <gary.jenkins@klowd.com>
2018-03-05 10:00:06 -0700

    Help System/Files

--- fuzion-cli.js
+++ fuzion-cli.js
@@ -2,29 +2,26 @@
 const fs = require("fs");
 const path = `${__dirname}/lib`;
 const argv = require("optimist")
-	.demand('o')
-	.alias('o','operation')
-	.describe('o',"Run a CLI operation.")
-	.usage(`fuzion-cli -o operation-name\nfuzion-cli -o "operation-name1|operation-name2|..."\nOperations:${listCliOperations()}`)
-	.demand(['o'])
+	.demand('op')
+	.usage(`\nfuzion-cli -op operation-name\nfuzion-cli -op operation-name --help\nfuzion-cli -op "operation-name1|operation-name2|..."\n\nOperations:${listCliOperations()}`)
 	.argv;
 
 function listCliOperations() {
 	let opFiles = fs.readdirSync(path);
 	opFiles = opFiles.filter(fileName => (fs.lstatSync(`${path}/${fileName}`).isFile() && fileName.match(/\.js$/)));
-	return opFiles.map(file => ' ' + file.replace('.js',''));
+	return opFiles.map(file => '\n\t' + file.replace('.js',''));
 }
 
-const ops = argv.o.split('|');
+const ops = argv.op.split('|');
 
 iterateOps(ops);
 
 async function iterateOps(ops) {
 	for(op of ops) {
-		console.log(`fuzion-cli -o ${op}`);
+		console.log(`fuzion-cli -op ${op}`);
 		try {
 			const opMod = require(`${path}/${op}`);
-			await opMod.cli();
+			await (argv.help) ? console.log(opMod.help) : opMod.cli();
 		} catch (error) {
 			console.log(`${error}`);
 			//console.log(error);
