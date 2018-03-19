974a83c22626b337ddd82ec6727fe0513e5fd5af
Gary Dean Jenkins <gary.jenkins@klowd.com>
2018-03-19 10:07:52 -0600

    Refactors . _ infront

---
+++ lib/template/sql/_create_uuid_func.sql
@@ -0,0 +1,16 @@
+USE `fuzion`;
+
+drop function if exists create_uuid;
+
+delimiter //
+create function create_uuid ()
+returns
+	varchar(32)
+begin
+	declare retv binary(16);
+	set @temp_uuid = uuid();
+	set @temp_reorder = CONCAT(SUBSTR(@temp_uuid, 15, 4),SUBSTR(@temp_uuid, 10, 4), SUBSTR(@temp_uuid, 1, 8),SUBSTR(@temp_uuid, 20, 4),SUBSTR(@temp_uuid, 25));
+	set retv = unhex(@temp_reorder);
+	return retv;
+end //
+delimiter ;
