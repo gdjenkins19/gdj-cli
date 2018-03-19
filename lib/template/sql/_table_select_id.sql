bc4f712922c4733966befe481685d77f7deb81cb
Gary Dean Jenkins <gary.jenkins@klowd.com>
2018-03-19 11:29:49 -0600

    _table_select_id refactor

--- lib/template/sql/_table_select_id.sql
+++ lib/template/sql/_table_select_id.sql
@@ -6,10 +6,11 @@ drop procedure if exists table_select_id;
 delimiter //
 create procedure table_select_id (
 	in in_table_name varchar(64),
-	in in_id varchar(64)
+	in in_id_name varchar(64),
+	in in_id varchar(32)
 )
 begin
-	SET @sql = CONCAT('SELECT * FROM ', in_table_name, ' WHERE `fuzion_', in_table_name, '_id` = unhex(\'', in_id, '\');');
+	SET @sql = CONCAT('SELECT * FROM ', in_table_name, ' WHERE `', in_id_name, '` = unhex(\'', in_id, '\');');
 	PREPARE statement FROM @sql;
 	EXECUTE statement;
 	DEALLOCATE PREPARE statement;
